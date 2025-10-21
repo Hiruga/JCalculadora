25c76df0781d60b662fd0b489f154ae7cc8f3648
Hiruga <hirugahasagui@gmail.com>
2025-10-21 20:18:50 -0300

    Implementando as operações matemáticas

--- src/CalculadoraView.java
+++ src/CalculadoraView.java
@@ -2,11 +2,16 @@ import javax.swing.*;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JPanel;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class CalculadoraView extends JFrame implements ActionListener {
+
+    private double n1 = 0;
+    private double n2 = 0;
+    private String operador = "";
+    private boolean esperando = false;
+    private boolean continuidade = false;
 
     private JPanel contentPane;
     private JButton divisaoButton;
@@ -26,36 +31,174 @@ public class CalculadoraView extends JFrame implements ActionListener {
     private JButton a3Button;
     private JButton virgulaButton;
     private JButton clearButton;
-    private JTextField resultado;
-
-    void soma(){
-        double n1;
-        double n2;
-        double result;
-    }
+    private JTextField display;
+    private JTextField opera;
+    private JButton deleteButton;
 
     public CalculadoraView() {
+        Operacoes operacoes = new Operacoes();
+
         setTitle("Calculadora");
         setContentPane(contentPane);
         setSize(400,300);
 
-        a0Button.addActionListener(e -> resultado.setText(resultado.getText() + "0"));
-        a1Button.addActionListener(e -> resultado.setText(resultado.getText() + "1"));
-        a2Button.addActionListener(e -> resultado.setText(resultado.getText() + "2"));
-        a3Button.addActionListener(e -> resultado.setText(resultado.getText() + "3"));
-        a4Button.addActionListener(e -> resultado.setText(resultado.getText() + "4"));
-        a5Button.addActionListener(e -> resultado.setText(resultado.getText() + "5"));
-        a6Button.addActionListener(e -> resultado.setText(resultado.getText() + "6"));
-        a7Button.addActionListener(e -> resultado.setText(resultado.getText() + "7"));
-        a8Button.addActionListener(e -> resultado.setText(resultado.getText() + "8"));
-        a9Button.addActionListener(e -> resultado.setText(resultado.getText() + "9"));
-
-        String textoAtual = resultado.getText();
-        virgulaButton.addActionListener(e-> {if (!textoAtual.contains(",")) resultado.setText(resultado.getText() + ",");
+        clearButton.addActionListener(e -> {
+            display.setText("");
+            n1 = 0;
+            n2 = 0;
+            operador = "";
+            esperando = false;
+            continuidade = false;
+            opera.setText("");
+        });
+        deleteButton.addActionListener(e -> {
+            String texto = display.getText();
+            if (!texto.isEmpty()){
+                texto = texto.substring(0, texto.length() - 1);
+                display.setText(texto);
+            }
+        });
+        virgulaButton.addActionListener(e-> {
+            String textoAtual = display.getText();
+            if (!textoAtual.contains("."))
+                display.setText(display.getText() + ".");
         });
 
-        somaButton.addActionListener(e -> resultado.setText(resultado.getText() + "+"));
-        //fazer as operações matemáticas
+        a0Button.addActionListener(e -> {
+            if (!continuidade){
+            display.setText(display.getText() + "0");
+        }else display.setText("0");
+        });
+        a1Button.addActionListener(e -> {
+            if (!continuidade){
+                display.setText(display.getText() + "1");
+            } else display.setText("1");
+            continuidade = false;
+        });
+        a2Button.addActionListener(e -> {
+            if (!continuidade){
+                display.setText(display.getText() + "2");
+            } else display.setText("2");
+            continuidade = false;
+        });
+        a3Button.addActionListener(e -> {
+            if (!continuidade){
+                display.setText(display.getText() + "3");
+            } else display.setText("3");
+            continuidade = false;
+        });
+        a4Button.addActionListener(e -> {
+            if (!continuidade){
+                display.setText(display.getText() + "4");
+            } else display.setText("4");
+            continuidade = false;
+        });
+        a5Button.addActionListener(e -> {
+            if (!continuidade){
+                display.setText(display.getText() + "5");
+            } else display.setText("5");
+            continuidade = false;
+        });
+        a6Button.addActionListener(e -> {
+            if (!continuidade){
+                display.setText(display.getText() + "6");
+            } else display.setText("6");
+            continuidade = false;
+        });
+        a7Button.addActionListener(e -> {
+            if (!continuidade){
+                display.setText(display.getText() + "7");
+            } else display.setText("7");
+            continuidade = false;
+        });
+        a8Button.addActionListener(e -> {
+            if (!continuidade){
+                display.setText(display.getText() + "8");
+            } else display.setText("8");
+            continuidade = false;
+        });
+        a9Button.addActionListener(e -> {
+            if (!continuidade){
+                display.setText(display.getText() + "9");
+            } else display.setText("9");
+            continuidade = false;
+        });
+
+        somaButton.addActionListener(e -> {
+            String vazio = display.getText();
+            if (!vazio.isEmpty()){
+            if (operador != "+") {
+                opera.setText("");
+                n1 = Double.parseDouble(display.getText());
+                opera.setText(display.getText()+ " + ");
+                display.setText("");
+                operador = "+";
+                esperando = true;
+            }}});
+        subtButton.addActionListener(e -> {
+            String vazio = display.getText();
+            if (!vazio.isEmpty()){
+            if (operador != "-") {
+                opera.setText("");
+                n1 = Double.parseDouble(display.getText());
+                opera.setText(display.getText()+ " - ");
+                display.setText("");
+                operador = "-";
+                esperando = true;
+            }}else{
+            display.setText("-");
+            operador = "-";
+            }
+        });
+        multButton.addActionListener(e -> {
+            String vazio = display.getText();
+            if (!vazio.isEmpty()){
+            if (operador != "*") {
+                opera.setText("");
+                n1 = Double.parseDouble(display.getText());
+                opera.setText(display.getText()+ " * ");
+                display.setText("");
+                operador = "*";
+                esperando = true;
+            }}});
+        divisaoButton.addActionListener(e -> {
+            if (operador != "/") {
+                String vazio = display.getText();
+                if (!vazio.isEmpty()){
+                opera.setText("");
+                n1 = Double.parseDouble(display.getText());
+                opera.setText(display.getText()+ " / ");
+                display.setText("");
+                operador = "/";
+                esperando = true;
+        }}});
+
+        calcularButton.addActionListener(e ->{
+            if (esperando){
+                n2 = Double.parseDouble(display.getText());
+                opera.setText(opera.getText() + display.getText()+ " = ");
+                double resultadoOperacao = 0;
+
+                switch (operador){
+                    case("+"):
+                        resultadoOperacao = operacoes.soma(n1, n2);
+                        break;
+                    case("-"):
+                        resultadoOperacao = operacoes.subtracao(n1, n2);
+                        break;
+                    case("*"):
+                        resultadoOperacao = operacoes.multiplicacao(n1, n2);
+                        break;
+                    case("/"):
+                        resultadoOperacao = operacoes.divisao(n1, n2);
+                }
+                display.setText(String.valueOf(resultadoOperacao));
+                operador = "";
+                esperando = false;
+                continuidade = true;
+            }
+        });
+
         setVisible(true);
     }
         @Override
--- src/Operacoes.java
+++ src/Operacoes.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Operacoes {
+
+    public double soma(double n1, double n2) {return n1 + n2;}
+    public double subtracao (double n1, double n2) {return n1 - n2;}
+    public double multiplicacao (double n1, double n2) {return n1 * n2;}
+    public double divisao (double n1, double n2) {
+        if (n2 == 0) throw new ArithmeticException("Divisão por zero");
+        return n1 / n2;
+    }
 }
